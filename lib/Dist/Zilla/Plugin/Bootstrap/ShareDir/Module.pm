ff5d5ba624f33edf08ad02524bb2ff5f6fe4d466
Kent Fredric <kentfredric@gmail.com>
2014-08-03 01:54:48 +1200

    Build results of 032bd10 (on master)

--- lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Module.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Module.pm
@@ -11,7 +11,7 @@ our $VERSION = '1.000000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moose;
+use Moose qw( with has around );
 use MooseX::AttributeShortcuts;
 
 with 'Dist::Zilla::Role::Bootstrap';
