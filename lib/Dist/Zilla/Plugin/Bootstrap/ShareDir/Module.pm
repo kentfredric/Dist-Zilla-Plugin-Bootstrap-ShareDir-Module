e99d27cb6735d3b16096108d5d2ec9f34c6d2fc3
Kent Fredric <kentfredric@gmail.com>
2017-03-06 19:41:56 +1300

    v1.001002
     [Bugfix]
     - Avoid testing issues under -Ddefault_inc_excludes_dot
     - Closes https://github.com/kentnl/Dist-Zilla-Plugin-Bootstrap-ShareDir-Module/issues/1
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001001, see misc/*.deps* for details
     - configure: ↓1
     - develop: +7 ↑4 -3 (suggests: ↑2)
     - runtime: ↓1 -2
     - test: ↓2
    
     [Internals]
     - Remove use of ConfigDumper

--- lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Module.pm
+++ lib/Dist/Zilla/Plugin/Bootstrap/ShareDir/Module.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Bootstrap::ShareDir::Module;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 # ABSTRACT: Use a share directory on your dist for a module during bootstrap
 
